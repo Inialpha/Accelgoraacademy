2071303d8ef12d2c8f2a592af74922f1c9ab5482
Initech <inimfonebong001@gmail.com>
2024-07-27 13:39:32 +0000

    Add course, about and testimonial section

--- view/src/pages/Home.tsx
+++ view/src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import Course from '../components/Courses/section'
+import About from '../components/About/section'
+import Testimonial from '../components/Testimonial'
 const Home = () => {
     return (
         <>
             Home
+            < Course />
+            < About />
+            < Testimonial />
         </>
     )
 }
