93c5a4f63c6552abbeacfbe701d147eae03eb93e
Rashnotech <rashnotech@gmail.com>
2024-07-31 14:26:21 +0100

    Added: contact component

---
+++ view/src/components/Contact/contact.tsx
@@ -0,0 +1,11 @@
+const Contact = () => {
+    return (
+        <>
+            <section>
+                <h2>Contact us</h2>
+            </section>
+        </>
+    )
+}
+
+export default Contact;
